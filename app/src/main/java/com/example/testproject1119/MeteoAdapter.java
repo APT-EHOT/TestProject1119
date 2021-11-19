d61ce2e7afb922c315efa3cd3229622b7478261d
a.matchin <a.matchin@prequelapp.com>
2021-11-19 14:00:27 +0300

    Finished work at recyclerview adapter

--- app/src/main/java/com/example/testproject1119/MeteoAdapter.java
+++ app/src/main/java/com/example/testproject1119/MeteoAdapter.java
@@ -1,34 +1,68 @@
 package com.example.testproject1119;
 
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class MeteoAdapter extends RecyclerView.Adapter<MeteoAdapter.ViewHolder> {
+
+    private List<WeatherItem> data;
+
+    public MeteoAdapter(final List<WeatherItem> data) {
+        this.data = data;
+    }
+
     @NonNull
     @Override
     public MeteoAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new ViewHolder(LayoutInflater
+            .from(parent.getContext())
+            .inflate(R.layout.item_meteo, parent, false));
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull MeteoAdapter.ViewHolder holder, int position) {
-
+        WeatherItem weatherItem = data.get(position);
+        holder.date.setText(weatherItem.dt_txt);
+        holder.temp.setText(Double.toString(weatherItem.main.temp));
+        holder.cloud.setText(weatherItem.weather.get(0).toString());
+        holder.wind.setText(Double.toString(weatherItem.wind.speed));
+        holder.pressure.setText(Double.toString(weatherItem.main.pressure));
+        holder.humidity.setText(Double.toString(weatherItem.main.humidity));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return data.size();
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
 
+        private View view;
+        private TextView date;
+        private TextView temp;
+        private TextView cloud;
+        private TextView wind;
+        private TextView pressure;
+        private TextView humidity;
 
         public ViewHolder(View itemView) {
             super(itemView);
-
+            view = itemView;
+            date = view.findViewById(R.id.date);
+            temp = view.findViewById(R.id.temp);
+            cloud = view.findViewById(R.id.cloud);
+            wind = view.findViewById(R.id.wind);
+            pressure = view.findViewById(R.id.pressure);
+            humidity = view.findViewById(R.id.humidity);
         }
     }
 }
