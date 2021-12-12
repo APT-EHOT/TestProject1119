a934dbdc41f3492beb8f8a440b6b4a198439919a
a.matchin <a.matchin@prequelapp.com>
2021-12-12 21:31:49 +0300

    RxJava implementation

--- app/src/main/java/com/example/testproject1119/MainActivity.java
+++ app/src/main/java/com/example/testproject1119/MainActivity.java
@@ -5,25 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.accounts.NetworkErrorException;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
     public RecyclerView recyclerView;
     public ProgressBar pb;
-    public WeatherResponse wr;
+    public TextView tv;
+    public List<WeatherItem> weatherData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,40 +44,55 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.list);
         pb = findViewById(R.id.progress);
-
-        Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl(MeteoApi.HOST)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build();
-
-        MeteoApi meteoApi = retrofit.create(MeteoApi.class);
-
-        Call<WeatherResponse> call = meteoApi.getMeteo(MeteoApi.query, MeteoApi.appId);
+        tv = findViewById(R.id.textView);
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
 
-        pb.postDelayed(() -> {
-            MeteoAdapter meteoAdapter = new MeteoAdapter(wr.list);
-            recyclerView.setHasFixedSize(true);
-            recyclerView.setLayoutManager(linearLayoutManager);
-            recyclerView.setAdapter(meteoAdapter);
-            pb.setVisibility(View.INVISIBLE);
-            recyclerView.setVisibility(View.VISIBLE);
-        }, 5000);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(MeteoApi.HOST)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create()) // Обязательно для взаимодействия Retrofit и RxJava
+                .build();
 
-        call.enqueue(new Callback<WeatherResponse>() {
-            @Override
-            public void onResponse(@NonNull Call<WeatherResponse> call, @NonNull Response<WeatherResponse> response) {
-                wr = response.body();
-                Log.d("MyLog", "Success");
-            }
+        MeteoApi meteoApi = retrofit.create(MeteoApi.class);
 
-            @Override
-            public void onFailure(@NonNull Call<WeatherResponse> call, @NonNull Throwable t) {
-                Log.e("MyLog", t.toString());
-            }
-        });
+        // Цепочка Rx вызовов
+        meteoApi.getMeteo(MeteoApi.query, MeteoApi.appId)
+                .subscribeOn(Schedulers.io()) // Подписка на источних данных в IO потоке
+                .observeOn(AndroidSchedulers.mainThread()) // Переключаем поток на главный UI поток для изменения интерфейса
+                .subscribe(new Observer<WeatherResponse>() { // Обработка событий подписки
+
+                    // Срабатывает при начале подписки
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) { }
+
+                    // Срабатывает каждое событие
+                    @Override
+                    public void onNext(@NonNull WeatherResponse weatherResponse) {
+                        weatherData = new ArrayList<>(weatherResponse.list);
+                    }
+
+                    // При ошибке
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        pb.setVisibility(View.INVISIBLE);
+                        tv.setVisibility(View.VISIBLE);
+                        Log.d("MyLog", "Ошибка");
+                    }
+
+                    // При завершении события
+                    @Override
+                    public void onComplete() {
+                        MeteoAdapter meteoAdapter = new MeteoAdapter(weatherData);
+                        recyclerView.setHasFixedSize(true);
+                        recyclerView.setLayoutManager(linearLayoutManager);
+                        recyclerView.setAdapter(meteoAdapter);
+                        pb.setVisibility(View.INVISIBLE);
+                        recyclerView.setVisibility(View.VISIBLE);
+                    }
+                } );
+
 
         System.out.println("test");
     }
--- app/src/main/java/com/example/testproject1119/MeteoApi.java
+++ app/src/main/java/com/example/testproject1119/MeteoApi.java
@@ -2,6 +2,7 @@ package com.example.testproject1119;
 
 import java.util.List;
 
+import io.reactivex.Observable;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
@@ -12,8 +13,9 @@ public interface MeteoApi {
     String query = "Moscow";
     String appId = "9cc4fb816c8ec8e389ece7e71d8cef8b";
 
+    // Создание Observable с данными о погоде
     @GET("data/2.5/forecast")
-    Call<WeatherResponse> getMeteo(@Query("q") String query,
-                                   @Query("appid") String appId);
+    Observable<WeatherResponse> getMeteo(@Query("q") String query,
+                                         @Query("appid") String appId);
 
 }
